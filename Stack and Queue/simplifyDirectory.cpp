5ed8a04ba32131295f74680bc33eab24f2467ffc
Ranjeet Kumar <krranjeet094@gmail.com>
2021-09-07 19:16:35 +0530

    Add files via upload
    Function to simplify the given directory into UNIX format.
    Example- 
    Input 1:
        A = "/home/"
    Output 1:
        "/home"
    
    Input 2:
        A = "/a/./b/../../c/"
    Output 2:
        "/c"

---
+++ Stack and Queue/simplifyDirectory.cpp
@@ -0,0 +1,53 @@
+void clearStack(stack<string> &s){
+    while(s.size() != 0){
+        s.pop();
+    }
+}
+
+string Solution::simplifyPath(string A) {
+    stack<string> st;
+    string dir;
+    string res;
+    res.append("/");
+
+    int len_A = A.length();
+
+    for (int i = 0; i < len_A; i++) {
+        dir.clear();
+        while (A[i] == '/')
+            i++;
+        while (i < len_A && A[i] != '/') {
+            dir.push_back(A[i]);
+            i++;
+        }
+
+        if (dir.compare("..") == 0) {
+            if (!st.empty())
+                st.pop();           
+        }
+
+        else if (dir.compare(".") == 0)
+            continue;
+        else if (dir.length() != 0)
+            st.push(dir);       
+    }
+
+stack<string> st1;
+    while (!st.empty()) {
+        st1.push(st.top());
+        st.pop();
+    }
+
+    while (!st1.empty()) {
+        string temp = st1.top();
+
+        if (st1.size() != 1)
+            res.append(temp + "/");
+        else
+            res.append(temp);
+ 
+        st1.pop();
+    }
+ 
+    return res;
+}
